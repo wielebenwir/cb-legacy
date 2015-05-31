ddaf64cc84efc4b44341e17cfbdf9a0f2328c68b
Florian Egermann <florian@macht-medien.de>
2015-05-31 16:09:08 +0200

    this file could be unnessecary

---
+++ commons-booking/admin/cb-codes/class-cb-codes.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *
+ * @package   Commons_Booking_Admin
+ * @author    Florian Egermann <florian@macht-medien.de>
+ * @license   GPL-2.0+
+ * @link      http://www.wielebenwir.de
+ * @copyright 2015 wielebenwir
+ */
+
+/**
+ * Plugin class. This class should ideally be used to work with the
+ * administrative side of the WordPress site.
+ *
+ * @package Commons_Booking_Codes
+ * @author  Your Name <email@example.com>
+ */
+class Commons_Booking_Codes {
+
+
+
+
+
+
+}
