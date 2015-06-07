83ca075d5bf4d0fa85c60a40dc7919def1667c2c
Christian Wenzel <christian@wielebenwir.de>
2015-06-07 18:34:50 +0200

    changed class and function structure

--- commons-booking/public/cb-bookings/class-commons-booking-frontend.php
+++ commons-booking/public/cb-bookings/class-commons-booking-frontend.php
@@ -5,6 +5,7 @@
  *
  * @package   Commons_Booking
  * @author    Florian Egermann <florian@macht-medien.de>
+ * @author    Christian Wenzel <christian@wielebenwir.de>
  * @license   GPL-2.0+
  * @link      http://www.wielebenwir.de
  * @copyright 2015 wielebenwir
@@ -30,33 +31,86 @@ class Commons_Booking_Frontend {
     const VERSION = '0.0.1';
 
     public function __construct() {
-    	$this->user_id = get_current_user_id();  // get user id
-
-    }
-
-
-    public function collect_booking_data($date_start, $date_end, $item_id) {
-
     	global $wpdb;
-    	$table_timeframe = $wpdb->prefix . 'cb_timeframes';
-    	$table_codes = $wpdb->prefix . 'cb_codes';
+    	$this->user_id = get_current_user_id();  // get user id
+    	$this->table_timeframe = $wpdb->prefix . 'cb_timeframes';
+    	$this->table_codes = $wpdb->prefix . 'cb_codes';
 
-    	if (!$this->user_id) {
+		if (!$this->user_id) {
     		// error message and exit
     	}
 
+    }
+
+/**
+ * Get location id based on booking-data and item
+ *
+ * @return array
+ */
+    public function get_booking_location_id( $date_start, $date_end, $item_id ) {
+    	
+    	global $wpdb;
 
     	// get location_id & item_id from timeframe-database
-    	 $booking_data[] = $wpdb->get_results($wpdb->prepare("SELECT item_id, location_id FROM " . $table_timeframe . " WHERE date_start <= '%s' AND date_end >= '%s'", $date_start, $date_end), ARRAY_A); // get dates from db
-    	 $timeframe_data;
+    	 $sqlresult = $wpdb->get_results($wpdb->prepare(
+    	 	"
+    	 	SELECT location_id 
+	 		FROM " . $this->table_timeframe . " 
+ 			WHERE  date_start <= '%s' AND date_end >= '%s' AND item_id = '%s'
+ 			", 
+ 			$date_start, $date_end, $item_id), ARRAY_A); // get dates from db
 
+    	 // @TODO: Insert check an error-handling if result-numer > 1
+
+    	 return $sqlresult[0];
+
+     }
+    
+ /**
+ * Get booking-code based on start date and item-id
+ *
+ * @return array
+ */   
+    public function get_booking_code_id( $date_start, $item_id ) {
+    	
+    	global $wpdb;
 
     	// get booking_code-id fromt codes database
-    	 $booking_data[] = $wpdb->get_results($wpdb->prepare("SELECT id FROM " . $table_codes . " WHERE booking_date = '%s' AND item_id = '%s'", $date_start, $item_id), ARRAY_A); // get dates from 
+    	 $sqlresult = $wpdb->get_results($wpdb->prepare(
+    	 	"
+    	 	SELECT id AS booking_code_id
+    	 	FROM " . $this->table_codes . " 
+    	 	WHERE booking_date = '%s' AND item_id = '%s'
+    	 	", 
+    	 	$date_start, $item_id), ARRAY_A); // get dates from 
     	 
+    	 // @TODO: Insert check an error-handling if result-numer > 1
 
-    	 return $booking_data;
+    	 return $sqlresult[0];
 
     }
- }
+
+
+ /**
+ * Store all booking relevant data into booking-table, set status pending
+ *
+ * @return array
+ */   
+    public function create_booking( $date_start, $date_end, $item_id ) {
+    	
+    	global $wpdb;
+
+    	$wpdb->insert( 
+			'table', 
+			array( 
+				'column1' => 'value1', 
+				'column2' => 123 
+			), 
+				array( 
+				'%s', 
+				'%d' 
+			) 
+		);
+
+	}
 ?>
