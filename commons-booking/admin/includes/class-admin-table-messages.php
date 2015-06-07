7de0698dfe7c0b2f20a4a44f1e527fcff19513ab
Florian Egermann <florian@macht-medien.de>
2015-06-07 22:46:47 +0200

    new function for adding messages to admin tables

---
+++ commons-booking/admin/includes/class-admin-table-messages.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *
+ * @package   Commons_Booking_Admin
+ * @author    Florian Egermann <florian@macht-medien.de
+ * @author    Christian Wenzel <christian@wielebenwir.de>
+ * @license   GPL-2.0+
+ * @link      http://www.wielebenwir.de
+ * @copyright 2015 wielebenwir
+ */
+
+/**
+ * Admin messages
+ *
+ * @package Commons Booking Admin Messages
+ * @author  Florian Egermann <florian@macht-medien.de>
+ */
+
+
+class Admin_Table_Message {
+
+    public $class = '';
+    public $message = '';
+
+    function __construct( $message, $class = 'updated' ){
+        $this->class = $class;
+        $this->message = $message;
+        $this->output();
+    }
+
+    function output(){
+        echo '<div id="message" class="' . $this->class .'"><p>' . $this->message . '</p></div>';
+    }
+}
+?>
