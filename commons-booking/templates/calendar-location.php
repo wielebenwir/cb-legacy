b41ee9a8a7ee00777e7507b391c0d55e44fc6692
Florian Egermann <florian@macht-medien.de>
2015-06-09 01:22:53 +0200

    template for location header on item page

---
+++ commons-booking/templates/calendar-location.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * Template for the booking calendar location header.
+ *
+ * @since   0.0.1
+ */
+?>
+<div class="cb location">
+  <h2><?php echo $location_name; ?></h2>
+  <p class="cb comment"><?php echo $timeframe_comment; ?></p>
+  <p class="cb contact"><?php echo $location_contact; ?></p>
+  <p class="cb geo"><?php echo ( 'lat: ' . $location_geo['latitude'] . ' long: ' . $location_geo['longitude'] ); ?></p>
+
+</div>
