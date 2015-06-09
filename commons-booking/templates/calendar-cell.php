84fb529ddd67cd51e032e050e0de82c30e21a8d8
Florian Egermann <florian@macht-medien.de>
2015-06-09 04:42:56 +0200

    added template for single cell

---
+++ commons-booking/templates/calendar-cell.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+ * Template for the booking calendar cell.
+ *
+ * @since   0.0.1
+ */
+?>
+<li class="<?php echo $day; ?>">
+  <span class="cb-date"><?php echo $date; ?></span>
+  <span class="cb-day"><?php echo $day; ?></span>
+
+
+</li>
