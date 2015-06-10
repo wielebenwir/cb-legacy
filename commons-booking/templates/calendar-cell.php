7b645ee16df67f38110c7c45d0579f96afc92b01
Florian Egermann <florian@macht-medien.de>
2015-06-10 17:20:40 +0200

    add tooltip to calendar cell

--- commons-booking/templates/calendar-cell.php
+++ commons-booking/templates/calendar-cell.php
@@ -5,7 +5,7 @@
  * @since   0.0.1
  */
 ?>
-<li id="<?php echo $counter; ?>" class="<?php echo $display_day . ' '. $class ; ?>">
+<li id="<?php echo $counter; ?>" class="tooltip <?php echo $display_day . ' '. $class ; ?>" title="This is my image tooltip message!">
   <span class="cb-date"><?php echo $display_date; ?></span>
   <span class="cb-day"><?php echo $display_day; ?></span>
 
