045a6b8805cc8efd05c4269e43d1d3a15804ded8
Florian Egermann <florian@macht-medien.de>
2015-06-14 12:11:39 +0200

    minor restructure

--- commons-booking/templates/calendar-bookingbar.php
+++ commons-booking/templates/calendar-bookingbar.php
@@ -18,30 +18,35 @@
 
 ?>
 <div id="cb-bookingbar">
+  <div id="cb-bookingbar-msg"></div>
+  <div class="inner">
 
-  <?php if ( is_user_logged_in() ) { ?>
-  <div class="cb-userinfo cb-small">
-    <?php 
-    global $current_user;
-      get_currentuserinfo();
-      echo ( __( 'Logged in as:' ));
-      echo $current_user->display_name;
-    ?>
-  </div>
-  <div id="date-start">
-    from
-  </div>
-  <div id="date-end">
-    date till
-  </div>
+  <div class="booking">
+   <div id="date-start">
+      from
+    </div>
+    <div id="date-end">
+      date till
+    </div>
 
-  <div id="cb-submit">
-    <a href="#" class="button cb-button">
-      Submit
-    </a>
+    <div id="cb-submit">
+      <a href="#" class="button cb-button">
+        Submit
+      </a>
+    </div>
   </div>
-  <?php } else { ?>
-    <p class="cb-big"><?php echo __( 'You have to be registered to book.' ); ?></p>
-    <a href="<?php echo wp_login_url(); ?>"><?php echo __( 'Login' ); ?></a> | <a href="<?php echo wp_registration_url(); ?>"><?php echo __( 'Register' ); ?></a>
-  <?php } ?>
+   <?php if ( is_user_logged_in() ) { ?>
+       <div class="cb-userinfo cb-small">
+      <?php 
+      global $current_user;
+        get_currentuserinfo();
+        echo ( __( 'Logged in as:' ));
+        echo $current_user->display_name;
+      ?>
+    </div>
+    <?php } else { ?>
+      <p class="cb-big"><?php echo __( 'You have to be registered to book.' ); ?></p>
+      <a href="<?php echo wp_login_url(); ?>"><?php echo __( 'Login' ); ?></a> | <a href="<?php echo wp_registration_url(); ?>"><?php echo __( 'Register' ); ?></a>
+    <?php } ?>
+  </div>
 </div>
