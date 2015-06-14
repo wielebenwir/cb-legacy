840855a94158fec4a5e1e1908f269ac2bb82b85d
Florian Egermann <florian@macht-medien.de>
2015-06-14 12:09:19 +0200

    no change

--- commons-booking/templates/calendar-cell.php
+++ commons-booking/templates/calendar-cell.php
@@ -8,6 +8,4 @@
 <li id="<?php echo $counter; ?>" class="tooltip <?php echo $display_day . ' '. $class ; ?>" title="This is my image tooltip message!">
   <span class="cb-date"><?php echo $display_date; ?></span>
   <span class="cb-day"><?php echo $display_day; ?></span>
-
-
 </li>
