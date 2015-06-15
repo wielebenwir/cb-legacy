8c39ab3776289082555b9c33afb7bba1e7637f92
Florian Egermann <florian@macht-medien.de>
2015-06-16 01:41:21 +0200

    added inputs fields for all values needed on the booking review page

--- commons-booking/templates/calendar-bookingbar.php
+++ commons-booking/templates/calendar-bookingbar.php
@@ -50,8 +50,14 @@
     <form id="booking-selection" action="<?php echo $this->target_url; ?>" method="post">
       <input type="hidden" name="date_start">
       <input type="hidden" name="date_end">
+      <input type="hidden" name="timeframe_id">
+      <input type="hidden" name="item_id">
+      <input type="hidden" name="location_id">
+      <input type="hidden" name="create" value="1">
       <?php wp_nonce_field('booking-review-nonce'); ?>
     </form>
+    <div id="data"></div>
+    <div id="debug"></div>
     <?php } else { ?>
       <p class="cb-big"><?php echo __( 'You have to be registered to book.' ); ?></p>
       <a href="<?php echo wp_login_url(); ?>"><?php echo __( 'Login' ); ?></a> | <a href="<?php echo wp_registration_url(); ?>"><?php echo __( 'Register' ); ?></a>
