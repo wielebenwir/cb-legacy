12a8a25b7b332af0d8518236a6fab73c5d781c33
Florian Egermann <florian@macht-medien.de>
2015-06-16 20:48:39 +0200

    templates & css

---
+++ commons-booking/templates/booking-code.php
@@ -0,0 +1,12 @@
+<?php 
+/**
+ * Template for Booking Code Display. 
+ *
+ */
+?>
+<div class="cb-headline"><?php echo  __( ' Your Booking Code' ); ?></div>
+<div class="cb-booking-code cb-box">
+  <div class="cb-big cb-booking-code">
+    <strong><?php echo $code ; ?></strong>
+  </div>
+</div>
