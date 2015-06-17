6f7b15d7abcfb86ea6ab45895f0576109e175cd9
Florian Egermann <florian@macht-medien.de>
2015-06-18 01:23:34 +0200

    minor indent

--- commons-booking/templates/booking-location.php
+++ commons-booking/templates/booking-location.php
@@ -6,10 +6,14 @@
  */
 ?>
 <div class="cb-headline"><?php echo  __( ' Location information: ' ); ?><?php echo $location['name']; ?></div>
-  <div class="cb-booking-location cb-box">
-  <div class="img" style="float:right;"><?php if ( has_post_thumbnail( $location['id'] ) ) { echo get_the_post_thumbnail( $location['id'], 'thumbnail' ); } ?></div>
+<div class="cb-booking-location cb-box">
+  <div class="img" style="float:right;">
+    <?php if ( has_post_thumbnail( $location['id'] ) ) { echo get_the_post_thumbnail( $location['id'], 'thumbnail' ); } ?>
+  </div>
   <div class="cb-adress">
     <?php echo implode(', ', $location['address']); ?>
   </div>
-  <div class="cb-contactinfo"> <?php echo $location['contact']; ?></div>
+  <div class="cb-contactinfo">
+    <?php echo $location['contact']; ?>
+  </div>
 </div>
