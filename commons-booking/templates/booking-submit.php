e20a64d715d37724e98674661340db2153976b23
Florian Egermann <florian@macht-medien.de>
2015-06-18 01:24:00 +0200

    indent

--- commons-booking/templates/booking-submit.php
+++ commons-booking/templates/booking-submit.php
@@ -4,9 +4,9 @@
 
 ?>
 <form id="confirm" method="POST">
-<input type="hidden" name="confirm" value="1">
-<input type="hidden" name="booking_id" value="<?php echo $booking_id; ?>">
-<?php wp_nonce_field('booking-confirm-nonce'); ?>
-<input type="submit" method="post" action="" value="<?php echo __( 'Confirm my booking' ); ?> ">
+  <input type="hidden" name="confirm" value="1">
+  <input type="hidden" name="booking_id" value="<?php echo $booking_id; ?>">
+  <?php wp_nonce_field('booking-confirm-nonce'); ?>
+  <input type="submit" method="post" action="" value="<?php echo __( 'Confirm my booking' ); ?> ">
 </form>
 
