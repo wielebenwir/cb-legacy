3cb1af9bd2a4198cef3c44470424a4505c7e7c1e
Florian Egermann <florian@macht-medien.de>
2015-06-19 02:04:43 +0200

    added item description custom metabox display on list

--- commons-booking/templates/items-list.php
+++ commons-booking/templates/items-list.php
@@ -5,11 +5,10 @@
  * @since   0.0.1
  */
 ?>
-<div class="item">
-  <h2><a href="<?php the_permalink(); ?>"><?php $item['the_title']; ?></a></h2>
-  <div class="left">
-    <?php if ( has_post_thumbnail( $item[ID] ) ) { echo get_the_post_thumbnail( $item[ID], 'thumbnail' ); 
-   } ?>
+<div class="cb-item cb-list">
+  <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+  <div class="align-right">
+    <?php if ( has_post_thumbnail( $post->ID ) ) { echo get_the_post_thumbnail( $post->ID, 'thumbnail' ); } ?>
  </div>
- <div class="right"><?php echo get_the_content( $post->ID ); ?></div>
+ <div class="right"><?php echo get_post_meta( get_the_ID(), 'commons-booking_item_descr', true ); //@TODO: PREFIX ?></div>
 </div>
