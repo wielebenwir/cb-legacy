9e70ae2c3eabcea8d319b52add73b4fb5c32e18c
Florian Egermann <florian@macht-medien.de>
2015-06-25 21:04:57 +0200

    minor list html

--- commons-booking/templates/items-list.php
+++ commons-booking/templates/items-list.php
@@ -6,9 +6,9 @@
  */
 ?>
 <div class="cb-item cb-list">
-  <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+  <div class="cb-headline"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
   <div class="align-right">
     <?php if ( has_post_thumbnail( $post->ID ) ) { echo get_the_post_thumbnail( $post->ID, 'thumbnail' ); } ?>
  </div>
- <div class="right"><?php echo get_post_meta( get_the_ID(), 'commons-booking_item_descr', true ); //@TODO: PREFIX ?></div>
+ <div class="right"><?php echo get_post_meta( get_the_ID(), 'commons-booking_item_descr', true );  //@TODO: PREFIX ?></div>
 </div>
