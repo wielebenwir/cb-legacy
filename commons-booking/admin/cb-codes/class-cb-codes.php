4dde7fe970fa7ddbdd6ca93fe2db899b654c78da
Florian Egermann <florian@macht-medien.de>
2015-06-28 03:02:39 +0200

    renamed file

--- commons-booking/admin/cb-codes/class-cb-codes.php
+++ commons-booking/admin/cb-codes/class-cb-codes.php
@@ -13,13 +13,203 @@
  * administrative side of the WordPress site.
  *
  * @package Commons_Booking_Codes
- * @author  Your Name <email@example.com>
+ * @author  Florian Egermann <email@example.com>
  */
 class Commons_Booking_Codes {
 
+  public $table_name;
+
+  public $csv;
+  public $item_id; // always set
+  public $date_start;
+  public $date_end;
+  public $date;
+  public $daterange_start;
+  public $timeframe_id;
+
+/**
+ * Constructor.
+ *
+ * @param $item_id 
+ * @param $date_start
+ * @param $date_end
+ *
+ */
+  public function __construct( $item_id ) {
+ 
+    // get Codes from Settings page
+    $settings = new Commons_Booking_Admin_Settings;
+    $this->csv = $settings->get( 'codes', 'codes_pool' );
+
+    global $wpdb;
+    $this->table_name = $wpdb->prefix . 'cb_codes';
+
+    $this->prefix = "commons-booking";
+
+    $this->daterange_start = date('Y-m-d', strtotime( '-30 days' )); // currentdate - 30 days
+
+    $this->item_id = $item_id;
+
+}
+
+public function set_timeframe ( $timeframe_id, $date_start, $date_end ) {
+
+    $this->timeframe_id = $timeframe_id;
+    $this->date_start = $date_start;
+    $this->date_end = $date_end;
+  }
+
+/**
+ * Get settings from backend.
+ */
+  public function split_csv( $csv ) {
+
+    $splitted = explode(",", $this->csv);
+    $splitted = preg_grep('#S#', array_map('trim', $splitted)); // Remove Empty
+    return ($splitted);
+  }
+
+/**
+ * Get all entries from the codes DB. Ignore dates earlier than daterange_start 
+ *
+ * @return array
+ */
+  public function get_codes( ) {
+    global $wpdb;
+    $codesDB = $wpdb->get_results($wpdb->prepare("SELECT * FROM $this->table_name  WHERE item_id = %s AND booking_date > $this->daterange_start", $this->item_id ), ARRAY_A); // get dates from db
+    $single = $this->split_csv( $codesDB );
+    return $codesDB;
+  }
+
+ /**
+ * Get code for date / item 
+ *
+ * @return array
+ */
+  public function get_code( $date ) {
+    global $wpdb;
+    $code = $wpdb->get_results($wpdb->prepare("SELECT * FROM $this->table_name  WHERE item_id = %s AND booking_date > $this->$date", $this->item_id ), ARRAY_A); // get dates from db
+    return $code;
+  }
 
 
+/**
+ * Compare timeframe dates and entries in the codes db 
+ * */
+  public function compare() {
+    $codesDB = $this->get_codes();
 
+    $tfDates = get_dates_between( $this->date_start, $this->date_end );
+    $codeDates = array();
 
+    foreach ( $codesDB as $entry ) {
+      array_push ($codeDates, $entry['booking_date']);
+    }
+    
+    $matched = array();
+    $missing = array();
+    $missingFlat = '';
 
+    for ( $i = 0; $i < count($tfDates); $i++ ) {
+
+      $index = array_search( $tfDates[ $i ], $codeDates );
+      $temp = array();
+      if ( ($index !== FALSE) ) {
+        $temp[ 'date'] = $tfDates[ $i ];
+        $temp[ 'code'] = $codesDB[ $index ]['bookingcode'];
+        array_push ($matched, $temp);
+      } else {
+        $temp[ 'date'] = $tfDates[ $i ];
+        array_push ($missing, $temp);
+      }
+    }
+    $this->matchedDates = $matched;
+    $this->missingDates = $missing;
+  }
+
+/**
+ * Handle the display of the dates/codes interface
+ */
+public function render() {
+
+  echo ( '<h2>Codes: ' . get_the_title( $this->item_id ) . '</h2>');
+
+  if ( $this->missingDates ) { 
+    ?>
+
+    <?php new Admin_Table_Message ( __('No codes generated or codes missing.', $this->prefix), 'error' ); ?>
+    <form id="codes" method="POST">
+      <input class="hidden" name="id" value="<?= $this->timeframe_id; ?>">  
+      <input class="hidden" name="generate" value="generate">
+      <input type="submit" value="<?php _e('Generate Codes', $this->prefix)?>" id="submit_generate" class="button-primary" name="submit_generate">
+    </form>
+
+    <?php
+    if (isset($_REQUEST['generate'])) {
+      $sql = $this->sql_insert( $this->item_id, $this->missingDates, $this->csvcodes );
+    }
+  } else { // no Codes missing?>
+    <?php   
+  } // end if $missingDates
+
+  $allDates = array_merge ($this->missingDates, $this->matchedDates);
+  $this->render_table( $allDates );
+}
+/**
+ * Render the dates/codes-table.
+ *
+ */
+public function render_table( $dates ) {
+  ?>
+  <table class="widefat striped">
+    <thead>
+      <tr>
+        <th><?php _e( 'Date' ); ?></th>
+        <th><?php _e( 'Code' ); ?></th>
+      </tr>
+    </thead>
+  <?php foreach ($dates as $row) {
+      if ( !isset($row[ 'code' ])) { 
+        $row[ 'code' ] = ('<span style="color:red">'. __( ' Missing Code', $this->prefix) .'</span>'); 
+      } ?>
+    <tr><td><?php _e( date( 'j.n.y', strtotime( $row[ 'date' ] ))); ?></td><td><?php _e( $row[ 'code' ] ); ?></td></tr>
+  <?php } // end foreach ?>
+  </table>
+  <?php
+}
+/**
+ * Add pointers.
+ * @TODO: check for security / split into prepare_sql and do_sql
+ *
+ * @param $itemid 
+ * @param $array list of dates
+ * @param $array list of codes
+ */
+private function sql_insert( $itemid, $array, $codes) {
+
+  new WP_Admin_Notice( __( 'Error Messages' ), 'error' );
+
+  global $wpdb;
+  $table_name = $wpdb->prefix . 'cb_codes'; 
+
+  shuffle( $codes ); // randomize array
+
+  if ( count( $codes ) < count( $array )) {
+    new Admin_Table_Message ( __('Not enough codes defined. Enter them in the Settings.', $this->prefix), 'error' );
+    return false;
+
+  }
+
+  $sqlcols = "item_id,booking_date,bookingcode";
+  $sqlcontents = array();
+  $sqlquery = '';
+  $count = count( $array );
+
+  for ( $i=0; $i < $count; $i++ ) {
+    array_push($sqlcontents, '("' . $itemid. '","' . $array[$i]['date'] . '","' . $codes[$i] . '")');
+  }
+  $sqlquery = 'INSERT INTO ' . $table_name . ' (' . $sqlcols . ') VALUES ' . implode (',', $sqlcontents ) . ';';
+
+  $wpdb->query($sqlquery);
+  }
 }
