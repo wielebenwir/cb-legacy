d70859834badf3acc715dddcf29e37a3e329f115
Florian Egermann <florian@macht-medien.de>
2015-06-28 23:15:59 +0200

    tablefilter script now automatically adapting to page (e.g. cb_timeframes)

--- commons-booking/admin/assets/js/tableFilter.js
+++ commons-booking/admin/assets/js/tableFilter.js
@@ -3,6 +3,8 @@
 
   $(function() {
 
+      var page = getUrlParameter('page');
+
     // Filters for Admin Table
 
     $( 'div.tablefilters select' ).each(function( ) {
@@ -10,9 +12,9 @@
       $( this ).change(function(){
         var filterID = $(this).val();
         if( filterID != '' ){
-          document.location.href = 'admin.php?page=cb_timeframes'+filterID;    
+          document.location.href = 'admin.php?page='+page+filterID;    
         } else {
-          document.location.href = 'admin.php?page=cb_timeframes';    
+          document.location.href = 'admin.php?page='+page;    
         }
       });    
     });
