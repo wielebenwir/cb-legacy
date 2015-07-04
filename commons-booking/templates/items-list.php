7283e4a30133f98f9372ef12763ac5d76806efa1
Florian Egermann <florian@macht-medien.de>
2015-07-04 18:11:08 +0200

    fixed: error if no future timeframes configured

--- commons-booking/templates/items-list.php
+++ commons-booking/templates/items-list.php
@@ -9,19 +9,24 @@
 
 <?php foreach ( $items as $item ) { ?>
 <div class="cb-list-item">
-<div class="cb-item cb-list">
-  <div class="cb-headline"><a href="<?php echo $item['permalink']; ?>"><?php echo $item['title']; ?></a></div>
-  <?php if ( isset( $item['thumb'] )) { ?><div class="align-right"><?php echo $item['thumb']; ?></div><? } ?>
-  <div class="right"><?php echo $item['description']; ?></div>
-</div>
-  <ul class="cb-list-item-timeframe">
-<?php foreach ( $item['location'] as $location ) { ?>
-<li class="cb-small">
-<span class="cb-date"><?php echo date ('d.m.', strtotime($location['date_start']) ); ?></span> - <span class="cb-date"> 
-  <?php echo date ( 'd.m.', strtotime( $location[ 'date_end' ]) ); ?> </span>: 
-  <?php echo $location[ 'name' ]; ?>,  <?php echo $location['address']['street']; ?>, <?php echo $location['address']['city'];  ?>
-  <span class="cb-action"><a href="<?php echo ( $item['permalink'] . '#timeframe' . $location[ 'name' ] );  ?>"> <?php echo __( 'Book here '); ?></a></span>
-</li><? } // end foreach location ?>
-  </ul>
-</div>
+  <div class="cb-item cb-list">
+    <div class="cb-headline"><a href="<?php echo $item['permalink']; ?>"><?php echo $item['title']; ?></a></div>
+    <?php if ( isset( $item['thumb'] )) { ?><div class="align-right"><?php echo $item['thumb']; ?></div><? } ?>
+    <div class="right"><?php echo $item['description']; ?></div>
+  </div>
+  <?php if ( isset( $item['location'] ) ) { ?>
+    <ul class="cb-list-item-timeframe">
+      <?php foreach ( $item['location'] as $location ) { ?>
+        <li class="cb-small">
+          <span class="cb-date"><?php echo date ('d.m.', strtotime($location['date_start']) ); ?></span> - <span class="cb-date"> 
+          <?php echo date ( 'd.m.', strtotime( $location[ 'date_end' ]) ); ?> </span>: 
+          <?php echo $location[ 'name' ]; ?>,  <?php echo $location['address']['street']; ?>, <?php echo $location['address']['city'];  ?>
+          <span class="cb-action"><a href="<?php echo ( $item['permalink'] . '#timeframe' . $location[ 'name' ] );  ?>"> <?php echo __( 'Book here '); ?></a></span>
+        </li>
+      <? } // end foreach location ?>
+    </ul>
+  <?php } // end if isset location ?>
+  </div>
 <? } // end foreach ?>
+</div>
+
