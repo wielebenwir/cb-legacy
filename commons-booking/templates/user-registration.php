8c06319da607d5887bfb424a6cb8a0d81a128af0
Florian Egermann <florian@macht-medien.de>
2015-07-10 00:25:52 +0200

    added tags for for internalization

--- commons-booking/templates/user-registration.php
+++ commons-booking/templates/user-registration.php
@@ -2,43 +2,46 @@
 
     <form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post">
     <div>
-    <label for="username">Username <strong>*</strong></label>
+    <label for="username"><?php echo __( 'Username', $this->plugin_slug ); ?></label>
     <input type="text" name="username" value="<?php echo ( isset( $_POST['username'] ) ? $this->username : null ); ?>">
     </div>
      
     <div>
-    <label for="password">Password <strong>*</strong></label>
+    <label for="password"><?php echo __( 'Password',  $this->plugin_slug ); ?></label>
     <input type="password" name="password" value="<?php echo ( isset( $_POST['password'] ) ? $this->password : null ); ?>">
     </div>
      
     <div>
-    <label for="email">Email <strong>*</strong></label>
+    <label for="email"><?php echo __( 'Email', $this->plugin_slug ); ?> </label>
     <input type="text" name="email" value="<?php echo ( isset( $_POST['email']) ? $this->email : null ); ?>">
     </div>
      
     <div>
-    <label for="firstname">First Name</label>
-    <input type="text" name="fname" value="<?php echo ( isset( $_POST['fname']) ? $this->first_name : null ); ?>">
+    <label for="firstname"><?php echo __( 'First Name', $this->plugin_slug ); ?></label>
+    <input type="text" name="first_name" value="<?php echo ( isset( $_POST['first_name']) ? $this->first_name : null ); ?>">
     </div>    
 
     <div>
-    <label for="lastname">Last Name</label>
-    <input type="text" name="lname" value="<?php echo ( isset( $_POST['lname']) ? $this->last_name : null ); ?>">
+    <label for="lastname"><?php echo __( 'Last Name', $this->plugin_slug ); ?></label>
+    <input type="text" name="last_name" value="<?php echo ( isset( $_POST['last_name']) ? $this->last_name : null ); ?>">
     </div>
      
     <div>
-    <label for="phone">Phone</label>
+    <label for="phone"><?php echo __( 'Phone', $this->plugin_slug ); ?></label>
     <input type="text" name="phone" value="<?php echo ( isset( $_POST['phone']) ? $this->phone : null ); ?>">
     </div>   
 
     <div>
-    <label for="address">Adress</label>
+    <label for="address"><?php echo __( 'Adress', $this->plugin_slug ); ?></label>
     <input type="text" name="address" value="<?php echo ( isset( $_POST['address']) ? $this->address : null ); ?>">
     </div>    
     <div>
-    <label for="terms_accepted">terms_accepted</label>
+    <label for="terms_accepted"><?php echo __( 'I accept the terms', $this->plugin_slug ); ?></label>
+    
     <input type="checkbox" name="terms_accepted" value="<?php echo ( isset( $_POST['terms_accepted']) ? $this->terms_accepted : null ); ?>">
+ 
+    <?php wp_nonce_field( 'create_user', 'user_nonce' ); ?>
+
     </div>
         <input type="submit" name="submit" value="Register"/>
-
     </form>
