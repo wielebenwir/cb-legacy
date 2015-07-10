a55f8ba0b444bb8d0277d0d22499b72f64526b96
Florian Egermann <florian@macht-medien.de>
2015-07-11 00:48:37 +0200

    new version from github

--- commons-booking/includes/fake-page.php
+++ commons-booking/includes/fake-page.php
@@ -1,13 +1,14 @@
 <?php
 
 //Based on https://coderwall.com/p/fwea7g
+//Update to work on last Wordpress versione
 
 if ( !class_exists( 'Fake_Page' ) ) {
 
     /**
      * Simple class that generate a fake page on the fly
      *
-     * @package   Commons_Booking
+     * @package   Plugin_Name
      * @author    Ohad Raz & Mte90 <mte90net@gmail.com>
      * @license   GPL-2.0+
      * @copyright 2014 
@@ -42,26 +43,35 @@ if ( !class_exists( 'Fake_Page' ) ) {
             $page_slug = $this->slug;
 
             //check if user is requesting our fake page
-            if ( count( $posts ) == 0 && (strtolower( $wp->request ) == $page_slug || $wp->query_vars[ 'page_id' ] == $page_slug) ) {
+            if ( count( $posts ) == 0 && (strtolower( $wp->request ) == $page_slug || ( isset($wp->query_vars[ 'page_id' ]) && $wp->query_vars[ 'page_id' ] == $page_slug ) ) ) {
                 //create a fake post
                 $post = new stdClass;
+                $post->ID = '-1';
                 $post->post_author = 1;
-                $post->post_name = $page_slug;
-                $post->guid = get_bloginfo( 'wpurl' . '/' . $page_slug );
-                $post->post_title = 'page title';
-                $post->post_content = "Fake Content";
-                $post->ID = '-' . rand();
-                $post->post_status = 'static';
-                $post->comment_status = 'closed';
-                $post->ping_status = 'closed';
-                $post->comment_count = 0;
                 //dates may need to be overwritten if you have a "recent posts" widget or similar - set to whatever you want
                 $post->post_date = current_time( 'mysql' );
                 $post->post_date_gmt = current_time( 'mysql', 1 );
+                $post->post_title = $this->args[ 'post_title' ];
+                $post->post_content = $this->args[ 'post_content' ];
+                $post->comment_status = 'closed';
+                $post->ping_status = 'closed';
+                $post->post_password = '';
+                $post->post_name = $page_slug;
+                $post->to_ping = '';
+                $post->pinged = '';
+                $post->modified = $post->post_date;
+                $post->modified_gmt = $post->post_date_gmt;
+                $post->guid = get_bloginfo( 'wpurl' . '/' . $page_slug );
+                $post->menu_order = 0;
+                $post->post_type = 'page';
+                $post->post_status = 'publish';
+                $post->post_mime_type = '';
+                $post->comment_count = 0;
+                $post->ancestors = array();
 
                 $post = ( object ) array_merge( ( array ) $post, ( array ) $this->args );
-                $posts = NULL;
-                $posts[] = $post;
+
+                $posts = array( $post );
 
                 $wp_query->is_page = true;
                 $wp_query->is_singular = true;
@@ -79,7 +89,7 @@ if ( !class_exists( 'Fake_Page' ) ) {
                 $wp_query->queried_object_id = $post->ID;
                 $wp_query->current_post = $post->ID;
             }
-
+            
             return $posts;
         }
 
