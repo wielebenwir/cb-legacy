cd81cfc60ce354058b540b19f686cf1dc0459316
Florian Egermann <florian@macht-medien.de>
2015-07-11 00:50:08 +0200

    minor cosmetics

--- commons-booking/templates/items-list.php
+++ commons-booking/templates/items-list.php
@@ -8,16 +8,16 @@
 
 
 <?php foreach ( $items as $item ) { ?>
-<div class="cb-list">
+<div class="cb-list cb-box">
   <div class="cb-list-item">
+      <?php if ( isset( $item['thumb'] )) { ?><div class="align-right"><?php echo $item['thumb']; ?></div><? } ?>
     <div class="cb-headline"><a href="<?php echo $item['permalink']; ?>"><?php echo $item['title']; ?></a></div>
-    <?php if ( isset( $item['thumb'] )) { ?><div class="align-right"><?php echo $item['thumb']; ?></div><? } ?>
     <div class="right"><?php echo $item['description']; ?></div>
   </div>
   <?php if ( isset( $item['location'] ) ) { ?>
     <ul class="cb-list-item-timeframe">
       <?php foreach ( $item['location'] as $location ) { ?>
-        <li class="cb-small">
+        <li class="cb-small cb-row">
           <span class="cb-date"><?php echo date ('d.m.', strtotime($location['date_start']) ); ?></span> - <span class="cb-date"> 
           <?php echo date ( 'd.m.', strtotime( $location[ 'date_end' ]) ); ?> </span>: 
           <?php echo $location[ 'name' ]; ?>,  <?php echo $location['address']['street']; ?>, <?php echo $location['address']['city'];  ?>
