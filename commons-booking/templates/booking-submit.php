bf277b13dafa8f18b5f75dc35fa0399134c7a43d
Florian Egermann <florian@macht-medien.de>
2015-07-11 00:49:00 +0200

    changed variable

--- commons-booking/templates/booking-submit.php
+++ commons-booking/templates/booking-submit.php
@@ -5,7 +5,7 @@
 ?>
 <form id="confirm" action="<?php the_permalink(); ?>" method="GET">
   <input type="hidden" name="confirm" value="1">
-  <input type="hidden" name="booking" value="<?php echo $encrypted; ?>">
+  <input type="hidden" name="booking" value="<?php echo $this->hash; ?>">
   <input type="submit" value="<?php echo __( 'Confirm my booking' ); ?> ">
 </form>
 
