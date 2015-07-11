4a16b883b5a67edb8cbc64b59af1d138b5a76173
Florian Egermann <florian@macht-medien.de>
2015-07-11 16:04:32 +0200

    Template for single item description

---
+++ commons-booking/templates/item_single-item_info.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * Template for item description.
+ *
+ * @since   0.3
+ */
+?>
+  <div class="cb-item cb-description">
+    <?php echo $item_descr_short; ?> 
+    <a href="#" class="cb-toggle"><?php echo __('Read more'); ?></a>
+    <div class="full showhide">
+      <?php echo $item_descr_full; ?>  
+    </div>
+  </div>
