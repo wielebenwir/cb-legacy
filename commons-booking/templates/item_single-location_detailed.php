49fc7e824e99d192b2942b550ca2034a547958cb
Florian Egermann <florian@macht-medien.de>
2015-07-12 02:49:51 +0200

    if timeframe comment is empty, remove the "-"

--- commons-booking/templates/item_single-location_detailed.php
+++ commons-booking/templates/item_single-location_detailed.php
@@ -7,7 +7,7 @@
 ?>
 <div class="cb-location">
   <span class="cb-date"><?php echo $timeframe_date; ?></span>
-  <div class="cb-location-name"><?php echo $location[ 'name']; ?> – <?php echo $timeframe_comment; ?></div>
+  <div class="cb-location-name"><?php echo $location[ 'name']; ?><?php if (!empty ( $timeframe_comment ) ) { echo ' - ' .$timeframe_comment; } ?></div>
   <div class="cb-address cb-row"><?php echo ( implode( ' ', $location[ 'address' ] ) ); ?></div>
   <?php if (!$location[ 'contact_hide' ] ) { 
     echo ( '<div class="cb-contact cb-row">' . $location[ 'contact' ] . '</div>' ); 
