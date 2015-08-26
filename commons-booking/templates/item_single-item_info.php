0a8c46d644bca9091029a28d7d66b3d097c32d48
Florian Egermann <florian@macht-medien.de>
2015-08-26 23:17:41 +0200

    fixed localization string

--- commons-booking/templates/item_single-item_info.php
+++ commons-booking/templates/item_single-item_info.php
@@ -7,7 +7,7 @@
 ?>
   <div class="cb-item cb-description">
     <?php echo $item_descr_short; ?> 
-    <a href="#" class="cb-toggle"><?php echo __('Read more'); ?></a>
+    <a href="#" class="cb-toggle"><?php echo __('Info...', 'commons-booking'); ?></a>
     <div class="full showhide">
       <?php echo $item_descr_full; ?>  
     </div>
