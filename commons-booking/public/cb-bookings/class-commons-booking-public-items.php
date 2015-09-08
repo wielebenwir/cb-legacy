fc9dc83d837eae5ea40849215bc1104cca4c4fb5
Florian Egermann <florian@macht-medien.de>
2015-09-08 21:35:42 +0200

    rewrite of items display to make shortcodes possible

--- commons-booking/public/cb-bookings/class-commons-booking-public-items.php
+++ commons-booking/public/cb-bookings/class-commons-booking-public-items.php
@@ -37,54 +37,96 @@ class Commons_Booking_Public_Items {
       // get timeframe data 
       $this->data = new Commons_Booking_Data;
 
-      // get list of items
-      $args = array( 'posts_per_page' => 10, 'post_type' => 'cb_items', 'orderby' => 'title', 'order' => 'DESC' );
-      $this->the_query = new WP_Query( $args );
+      $this->defaults = array( 
+        'p' => '',
+        'cat' => '',
+        'posts_per_page' => 10, 
+        'post_type' => 'cb_items', 
+        'orderby' => 'title', 
+        'order' => 'DESC'
+      );
     }
 
-    public function get_Items() {
+  /**
+   * Get items
+   * 
+   * @since     0.4.5
+   * @param     mixed
+   * @return    array
+   */
+  public function get_Items( $the_query ) {
 
-      $content = array();
+    $content = array();
 
-      $query = $this->the_query;
+    if ( $the_query->have_posts() ) {
+      while ( $the_query->have_posts() ) {
+        
+        $the_query->the_post();
+        $item_id = get_the_ID();
 
-      if ( $query->have_posts() ) {
-        while ( $query->have_posts() ) {
-          
-          $query->the_post();
-          $item_id = get_the_ID();
+        $content [$item_id]['permalink'] =  get_the_permalink();
+        $content [$item_id]['title'] =  get_the_title();
+        if ( has_post_thumbnail( $item_id ) ) {  $content [$item_id]['thumb'] = get_the_post_thumbnail( $item_id, 'thumbnail' ); }
+        $content [$item_id]['description'] =  get_post_meta( get_the_ID(), 'commons-booking_item_descr', true );
 
-          $content [$item_id]['permalink'] =  get_the_permalink();
-          $content [$item_id]['title'] =  get_the_title();
-          if ( has_post_thumbnail( $item_id ) ) {  $content [$item_id]['thumb'] = get_the_post_thumbnail( $item_id, 'thumbnail' ); }
-          $content [$item_id]['description'] =  get_post_meta( get_the_ID(), 'commons-booking_item_descr', true );
+        $timeframes = $this->data->get_timeframes( $item_id  );
 
-          $timeframes = $this->data->get_timeframes( $item_id  );
+        if ( $timeframes ) {
+          foreach ( $timeframes as $tf ) {
+            $location = $this->data->get_location ( $tf ['location_id'] );
 
-          if ( $timeframes ) {
-            foreach ( $timeframes as $tf ) {
-              $location = $this->data->get_location ( $tf ['location_id'] );
-    
-              $content [$item_id]['location'][$tf ['location_id']] = $location;
-              $content [$item_id]['location'][$tf ['location_id']]['date_start'] = $tf ['date_start'];
-              $content [$item_id]['location'][$tf ['location_id']]['date_end'] = $tf ['date_end'];
-            } // end foreach
-          } 
+            $content [$item_id]['location'][$tf ['location_id']] = $location;
+            $content [$item_id]['location'][$tf ['location_id']]['date_start'] = $tf ['date_start'];
+            $content [$item_id]['location'][$tf ['location_id']]['date_end'] = $tf ['date_end'];
+          } // end foreach
         }
-      } else {
-        return __( ' Sorry, nothing found.');
       }
-       wp_reset_postdata();
+    } else {
+      return __( ' Sorry, nothing found.');
+    }
+     wp_reset_postdata();
        return $content; 
     }
 
-    public function items_render() {
+  /**
+   * Merge supplied arguments with defaults
+   * 
+   * @TODO move this to main file so other classes can use it.
+   *
+   * @since     0.4.5
+   * @param     array
+   * @return    array
+   */
+  public function merge_args( $args = array() ){
 
-      echo ("hello");
-
-      $items = $this->get_Items();
-      include (commons_booking_get_template_part( 'items', 'list', FALSE ));
-
+    //sort out whether defaults were supplied or just the array of search values
+    if( empty( $args ) ){
+      $array = $this->defaults;
+      
+      }else{
+        $array = array_merge($this->defaults, $args);
+      }
+      $array = array_filter( $array, 'strlen' ); // remove empty keys
+      return ($array);
     }
+  /**
+   * Output the item list
+   * 
+   * @since     0.4.5
+   * @param     args array
+   * @return    array
+   */
+  public function output( $args = array() ) {
+   
+    $queryargs = $this->merge_args($args);  
+    $query = new WP_Query( $queryargs );
+    $items = $this->get_Items( $query );
+
+
+    if ( is_array ($items) ) { // if result
+      include (commons_booking_get_template_part( 'items', 'list', FALSE ));
+      }
+
   }
+}
 ?>
