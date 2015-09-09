5331f4ce0c63cf9d8ca6100b19e8f488bd0a3d03
Florian Egermann <florian@macht-medien.de>
2015-09-09 14:08:14 +0200

    removed "info/more" button (page content is rendered anyway)

--- commons-booking/templates/item_single-item_info.php
+++ commons-booking/templates/item_single-item_info.php
@@ -7,10 +7,4 @@
 ?>
   <div class="cb-item cb-description">
     <?php echo $item_descr_short; ?> 
-    <?php if ( !empty ($item_descr_full) ) { ?>
-      <a href="#" class="cb-toggle"><?php echo __('Info...', 'commons-booking'); ?></a>
-      <div class="full showhide">
-        <?php echo $item_descr_full; ?>  
-      </div>
-    <?php } ?>
   </div>
