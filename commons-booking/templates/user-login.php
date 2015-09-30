5cd7788119d8fd29258996a3e9e8d52db77a85dd
Florian Egermann <florian@macht-medien.de>
2015-09-30 22:14:15 +0200

    addresses Missing Translations, Missing Blanks in  #52

--- commons-booking/templates/user-login.php
+++ commons-booking/templates/user-login.php
@@ -1,7 +1,7 @@
 
 
 <div class="cb-headline"><?php echo __('Welcome, stranger!', $this->plugin_slug ); ?> </div>
-<p><?php echo __('If you don´t have an account, please register here: ', $this->plugin_slug ); ?><a href="<?php echo wp_registration_url(); ?>">Register</a></p>
+<p><?php echo __('If you don´t have an account, please register here: ', $this->plugin_slug ); ?><a href="<?php echo wp_registration_url(); ?>"><?php echo __( 'Register', $this->plugin_slug); ?></a></p>
 <div class="cb-login cb-box">
    <?php wp_login_form( ); ?>
  </div>
