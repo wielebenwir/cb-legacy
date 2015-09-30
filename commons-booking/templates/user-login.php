fdcb7ede97871643d91e80b33f8200d694c2a025
Florian Egermann <florian@macht-medien.de>
2015-09-30 23:11:11 +0200

    Added link to WP Passwort recovery page. see #56

--- commons-booking/templates/user-login.php
+++ commons-booking/templates/user-login.php
@@ -2,6 +2,9 @@
 
 <div class="cb-headline"><?php echo __('Welcome, stranger!', $this->plugin_slug ); ?> </div>
 <p><?php echo __('If you don´t have an account, please register here: ', $this->plugin_slug ); ?><a href="<?php echo wp_registration_url(); ?>"><?php echo __( 'Register', $this->plugin_slug); ?></a></p>
+<p><?php echo __('If you have lost your password, click here: ', $this->plugin_slug ); ?><a href="<?php echo wp_lostpassword_url(); ?>" title="Lost Password"><?php echo __( 'Lost Password', $this->plugin_slug); ?></a>
+</p>
 <div class="cb-login cb-box">
+
    <?php wp_login_form( ); ?>
  </div>
