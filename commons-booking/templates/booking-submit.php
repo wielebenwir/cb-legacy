0ba6cfd5df57b02486264442a06dc7d36741cdd8
Florian Egermann <florian@macht-medien.de>
2015-10-01 19:19:14 +0200

    fixed missing translation strings

--- commons-booking/templates/booking-submit.php
+++ commons-booking/templates/booking-submit.php
@@ -6,6 +6,6 @@
 <form id="confirm" action="<?php the_permalink(); ?>" method="GET">
   <input type="hidden" name="confirm" value="1">
   <input type="hidden" name="booking" value="<?php echo $this->hash; ?>">
-  <input type="submit" value="<?php echo __( 'Confirm my booking' ); ?> ">
+  <input type="submit" value="<?php echo __( 'Confirm my booking', 'commons-booking' ); ?> ">
 </form>
 
