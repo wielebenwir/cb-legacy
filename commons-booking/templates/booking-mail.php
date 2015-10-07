4afbb07958a43dd3bf8a338e7af01c3882403e4c
Florian Egermann <florian@macht-medien.de>
2015-10-07 19:54:14 +0330

    booking mail template

--- commons-booking/templates/booking-mail.php
+++ commons-booking/templates/booking-mail.php
@@ -22,7 +22,7 @@
 <?php //USER ?>
 <div class="cb-headline"><?php echo __( ' Your information ' ); ?></div>
 <div class="cb-booking-user cb-box">
-  <div><?php echo __( ' Full name: ' ); ?><strong><?php echo $user['name']; ?></strong></div>
+  <div><?php echo __( ' Full name: ' ); ?><strong><?php echo $user['first_name']; ?> <?php echo $user['last_name']; ?></strong></div>
   <div><?php echo __( ' Email: ' ) ?><strong><?php echo $user['email']; ?></strong></div>
   <div><?php echo __( ' Address: ' ) ?><strong><?php echo $user['address']; ?></strong></div>
   <div><?php echo __( ' Phone number: ' ) ?><strong><?php echo $user['phone']; ?></strong></div>
