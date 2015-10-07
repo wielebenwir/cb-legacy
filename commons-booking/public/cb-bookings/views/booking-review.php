56703da9800789fc6b2e1227911b6e63e9aef4ca
Florian Egermann <florian@macht-medien.de>
2015-10-07 19:45:59 +0330

    Replaced user_name with  first_name last_name

--- commons-booking/public/cb-bookings/views/booking-review.php
+++ commons-booking/public/cb-bookings/views/booking-review.php
@@ -25,7 +25,7 @@
 <?php //USER ?>
 <div class="cb-headline"><?php echo __( ' Your information ', 'commons-booking' ); ?></div>
 <div class="cb-booking-user cb-box">
-  <div><?php echo __( 'Full name:', 'commons-booking' ); ?> <strong><?php echo $this->b_vars['user_name']; ?></strong></div>
+  <div><?php echo __( 'Full name:', 'commons-booking' ); ?> <strong><?php echo ( $this->b_vars['first_name'] . ' ' . $this->b_vars['last_name'] ); ?></strong></div>
   <div><?php echo __( 'Email:', 'commons-booking' ); ?> <strong><?php echo $this->b_vars['user_email']; ?></strong></div>
   <div><?php echo __( 'Address:', 'commons-booking' ); ?> <strong><?php echo $this->b_vars['user_address']; ?></strong></div>
   <div><?php echo __( 'Phone number:', 'commons-booking' ); ?> <strong><?php echo $this->b_vars['user_phone']; ?></strong></div>
