749ee00af069ae04bfeb9a241384d7685167a6fa
Florian Egermann <florian@macht-medien.de>
2016-03-20 20:13:14 +0100

    cleanup template functions

--- commons-booking/includes/template.php
+++ commons-booking/includes/template.php
@@ -5,49 +5,15 @@
  * Based on WooCommerce function<br>
  *
  * @package   Commons_Booking
- * @author  Mte90 <mte90net@gmail.com>
+ * @author  Florian Egermann
  * @license   GPL-2.0+
- * @copyright 2014 
- * @since    0.0.1
+ * @copyright 2016
+ * @since    0.6
  */
-function commons_booking_get_template_part( $slug, $name = '', $include = true ) {
-	$template = '';
-	$path = plugin_dir_path( realpath( dirname( __FILE__ ) ) ) . 'templates/';
-	$plugin = Commons_Booking::get_instance();
-	$plugin_slug = $plugin->get_plugin_slug().'/';
-	
-	// Look in yourtheme/slug-name.php and yourtheme/commons-booking/slug-name.php
-	if ( $name ) {
-		$template = locate_template( array( "{$slug}-{$name}.php", $plugin_slug . "{$slug}-{$name}.php" ) );
-	} else {
-		$template = locate_template( array( "{$slug}.php", $plugin_slug . "{$slug}.php" ) );
-	}
-	
-	// Get default slug-name.php
-	if ( !$template && $name && file_exists( $path . "{$slug}-{$name}.php" ) ) {
-		$template = $path . "{$slug}-{$name}.php";
-	}
-
-	// If template file doesn't exist, look in yourtheme/slug.php and yourtheme/woocommerce/slug.php
-	if ( !$template ) {
-		$template = locate_template( array( "{$slug}.php", $plugin_slug . "{$slug}.php" ) );
-	}
-
-	// Allow 3rd party plugin filter template file from their plugin
-	//$template = apply_filters( 'pn_get_template_part', $template, $slug, $name );
-
-	if ( $template && $include === true ) {
-		load_template( $template, false );
-		// include ($template);
-	} else if($template && $include === false ) {
-		return $template;
-	}
-}
 
 function cb_get_template_part( $template = '', $attributes = null, $buffer = FALSE ) {
 	$path = plugin_dir_path( realpath( dirname( __FILE__ ) ) ) . 'templates/';
-	$plugin = Commons_Booking::get_instance();
-	$plugin_slug = $plugin->get_plugin_slug();
+	$plugin_slug = 'commons-booking';
 	
 	// Look in yourtheme/slug-name.php and yourtheme/plugin-name/slug-name.php
   if ( ! $attributes ) {
