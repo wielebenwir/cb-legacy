27bcce61ba597322bfda3bee0f819432ec06bdf2
Florian Egermann <florian@macht-medien.de>
2016-03-20 20:14:02 +0100

    timeframe-list html output

--- commons-booking/templates/item-list-timeframes.php
+++ commons-booking/templates/item-list-timeframes.php
@@ -6,13 +6,11 @@
  */
 ?>
 <?php // timeframes ?>
-   <div class="cb-box">   
+
 <?php foreach ( $attributes['timeframes'] as $tf ) { ?>
-    <div class="cb-timeframe cb-row cb-small">
-      <span class="cb-date"><?=$tf['date_range'] ?></span>
-        <?=$tf['name'] ?>
-        <span class="cb-timeframe-description"><?=$tf['timeframe_title'] ?></span>
-        <span class="cb-button"><a href="<?php echo ( the_permalink() . '#timeframe' . $tf[ 'timeframe_id' ] );  ?>"> <?php echo __( 'Book here', $attributes['plugin_slug'] ); ?></a></span>
-    </div>
-<?php } // end foreach: timeframes ?>
+  <div class="cb-box">
+      <span class="cb-button"><a href="<?php echo ( the_permalink() . '#timeframe' . $tf[ 'timeframe_id' ] );  ?>"> <?php echo __( 'Book here', $attributes['plugin_slug'] ); ?></a></span>
+      <span class="cb-timeframe-title"><?=$tf['timeframe_title'] ?></span>
+      <span class="cb-date"><?=$tf['date_range'] ?></span> – <span class=""><?=$tf['name'] ?></span>  
   </div>
+<?php } // end foreach: timeframes ?>
