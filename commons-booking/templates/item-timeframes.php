ac51312b19d373f437e0b54d718b5295c797ab6f
Florian Egermann <florian@macht-medien.de>
2016-04-01 18:40:10 +0200

    minor html structure

--- commons-booking/templates/item-timeframes.php
+++ commons-booking/templates/item-timeframes.php
@@ -5,11 +5,11 @@
  * @since   0.5
  */
 ?>
-
+<div class="cb-timeframes-wrapper">
 <?php // timeframes ?>
 <?php foreach ( $attributes['timeframes'] as $tf ) { ?>
   <a name="timeframe<?= $tf['timeframe_id'] ?>"></a>
-   <div class="cb-timeframe" data-tfid="<?= $tf['timeframe_id'] ?>" data-itemid="<?=$attributes['item']['ID'] ?>" data-locid="<?= $tf['location_id'] ?>">   
+   <div class="cb-timeframe" id="<?= $tf['timeframe_id'] ?>" data-tfid="<?= $tf['timeframe_id'] ?>" data-itemid="<?=$attributes['item']['ID'] ?>" data-locid="<?= $tf['location_id'] ?>">   
     <div class="cb-location">
       <span class="cb-date"><?=$tf['date_range'] ?></span>
       <div class="cb-location-name">
@@ -21,14 +21,16 @@
     </div>
     <div id ="timeframe_<?=$tf['timeframe_id'] ?>" class="cb_timeframe_form">
         <ul class="cb-calendar">
-          <?php // calendar cells ?>
+          <?php // calendar cells ?>            
           <?php foreach ( $tf['calendar'] as $cell ) { ?>
-            <li id="<?=$cell['id'] ?>" class="cb-tooltip <?=$cell['weekday_code'] ?> <?=$cell['status'] ?>">
-              <span class="cb-date"><?=$cell['date_short'] ?></span>
+            <li id="<?=$cell['id'] ?>" class="cb-tooltip <?=$cell['weekday_code'] ?> <?=$cell['status'] ?>"><div class="cb-cal-inner"
+              ><span class="cb-date"><?=$cell['date_short'] ?></span>
               <span class="cb-day"><?=$cell['day_short'] ?></span>
+            </div>
             </li>
           <?php } // end foreach: cell ?>
         </ul>
     </div>
   </div>
 <?php } // end foreach: timeframes ?>
+</div>
