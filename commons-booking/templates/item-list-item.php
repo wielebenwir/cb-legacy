c3af2bd0d3a44aedb01f51aa8abed8a0a6fc410c
Florian Egermann <florian@macht-medien.de>
2016-04-02 14:28:30 +0200

    If no item short description provided, show a message.

--- commons-booking/templates/item-list-item.php
+++ commons-booking/templates/item-list-item.php
@@ -1,3 +1,10 @@
+<?php 
+/**
+ * Template: Item Description in item list view. 
+ *
+ * @since   0.6
+ */
+?>
 <div class="cb-list-item">
-   <?php echo ($attributes['commons-booking_item_descr'][0]); ?>
+   <?php echo ( isset( $attributes['commons-booking_item_descr'][0] ) ) ? $attributes['commons-booking_item_descr'][0] : __('No description provided.', 'commons-booking'); ?>
 </div>
