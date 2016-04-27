951efc7f8bb38bdcb90e2debbc251dc303ca6baf
Florian Egermann <florian@macht-medien.de>
2016-04-27 15:50:56 +0200

    Minor CSS rework

--- commons-booking/templates/item-list-timeframes-compact.php
+++ commons-booking/templates/item-list-timeframes-compact.php
@@ -11,7 +11,7 @@
     <div class="cb-row" id="timeframe_id_<?=$tf[ 'timeframe_id' ]; ?>">
         <span><a href="<?php echo ( the_permalink($attributes['item']['ID']) . '#timeframe' . $tf[ 'timeframe_id' ] );  ?>" class="cb-button align-right"> <?php echo __( 'Book here', 'commons-booking'); ?></a></span>
         <span class="cb-date"><?=$tf['date_range'] ?></span> <span class="cb-timeframe-title"><?=$tf['timeframe_title'] ?></span>
-        <span class="cb-big cb-location-name"><?=$tf['name'] ?></span>
+        <span class="cb-location-name"><?=$tf['name'] ?></span>
         <span class="cb-address"><?=$tf['address'] ?></span>
     </div>
   <?php } // end foreach: timeframes ?>
