5dd45693e71ea29e8a0db5e71fa4be57da9983da
Florian Egermann <florian@macht-medien.de>
2016-04-27 15:50:32 +0200

    cleanup

--- commons-booking/templates/booking-review-cancel.php
+++ commons-booking/templates/booking-review-cancel.php
@@ -10,5 +10,5 @@
   <input type="hidden" name="cancel" value="1">
   <input type="hidden" name="page_id" value="<?php echo $attributes['page_confirmation']; ?>">
   <input type="hidden" name="booking" value="<?php echo $attributes['hash']; ?>">
-    <input type="submit" id="wp-submit" class="cb-button warning" value="<?php echo __( 'Cancel my booking', 'commons-booking' ); ?>" >
+  <input type="submit" id="wp-submit" class="cb-button warning" value="<?php echo __( 'Cancel my booking', 'commons-booking' ); ?>" >
 </form>
