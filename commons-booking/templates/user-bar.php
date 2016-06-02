6129bd181ca46856e647732bb0f3a3480ed02253
Florian Egermann <florian@macht-medien.de>
2016-06-02 21:51:42 +0200

    FIX Issue 118: Logout-Button has button styling now.

--- commons-booking/templates/user-bar.php
+++ commons-booking/templates/user-bar.php
@@ -10,14 +10,14 @@
 
   $option = get_option( 'commons-booking-settings-pages');
   $id = $option['commons-booking_user_bookings_page_select'];
-  $url = get_permalink($id);
+  $url = get_permalink( $id );
 
   $current_user = wp_get_current_user();
   printf(__('Aloha, %s. ', 'commons-booking' ), $current_user->user_firstname);
   echo ('<span class="align-right">');
     printf(__('<a href="%s" class="cb-button">My Bookings</a> ', 'commons-booking' ),  $url );
-    printf(__('<a href="%s" class="cb-button">My Profile</a> ', 'commons-booking' ),  get_edit_profile_url() );
-    printf(__('<a href="%s" class="cb-button">Logout</a>', 'commons-booking' ), wp_logout_url( home_url() ) );
+    printf(__('<a href="%s" class="cb-button">My Profile</a> ', 'commons-booking' ),  get_edit_profile_url() );    
+    printf(__('<a href="%s" class="cb-button">Logout</a> ', 'commons-booking' ),  wp_logout_url( home_url() ) );
   echo ('</span>');
 
 ?>
