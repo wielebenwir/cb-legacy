b8fe5471c6f388ca493c6348063025836b7e7f78
Florian Egermann <florian@macht-medien.de>
2020-12-04 15:14:00 +0000

    Removed uninstall.php because i threw an error.

--- commons-booking.php
+++ commons-booking.php
@@ -94,7 +94,6 @@ require_once( plugin_dir_path( __FILE__ ) . 'admin/cb-widgets/cb-category-widget
 
 /*
  * Register hooks that are fired when the plugin is activated or deactivated.
- * When the plugin is deleted, the uninstall.php file is loaded.
  *
  */
 
